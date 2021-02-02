1d3df21c63ea326fce84da9509fb0a9a0e0a862e
Lolark Nath Sudama <76028604+lolark@users.noreply.github.com>
2021-02-02 10:15:53 +0530

    Add files via upload

---
+++ pp003.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+{
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d",&num);
+
+    if ( num & 1 == 1 )
+       printf("%d is odd.", num);
+    else
+       printf("%d is even.", num);
+
+    return 0;
+}
