f730c9cb3691d744c21dc29669651a50deb9fb6e
Lolark Nath Sudama <76028604+lolark@users.noreply.github.com>
2021-02-02 10:55:54 +0530

    Update pp009.c

--- pp009.c
+++ pp009.c
@@ -1,3 +1,5 @@
+//pp009: Write a program having a function iseven() which takes a number as input and returns 1 if the number is even else returns 0. Create the main() function to input a number and check it to be even or odd using iseven() function.
+
 #include<stdio.h>
 int iseven(int x)
 {
